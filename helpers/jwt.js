8b61221d94e260751237fb7cc9571941457593b1
Dehiker <dehiker80@gmail.com>
2023-08-03 14:03:00 -0400

    Ajuste 30 dias tiken

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -11,7 +11,7 @@ const generarJWT = (uid)=>{
         // el sign es para crearlo
         // lo que se va a firmar es el payload
         jwt.sign(payload, process.env.JWT_SECRET?process.env.JWT_SECRET:"clavesecretaindecifrable", {
-            expiresIn: '12h'
+            expiresIn: '30d'
         }, (err, token)=>{
             if(err){
                 console.log(err);
